ad41e318fb4b22aa1d7be51126fe9e6943a41055
rajeeva <rajeevabp92@gmail.com>
2024-01-31 17:06:27 +0530

    rearrrange the code

--- src/movieapp/getresults.js
+++ src/movieapp/getresults.js
@@ -1,21 +1,23 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { useSelector } from "react-redux";
+import { Col, Row, Card } from "antd";
 import ErrorPage from "../error/errorpage";
 import ListComp from "../core/list";
 import Loader from "../core/loader";
-import { Col, Row } from "antd";
 import "./movieapp.css";
 
 const GetMovieAppResults = () => {
   const searchSongValue = useSelector((state) => state.movieapp.searchvalue);
-  const [songValue, setSongValue] = React.useState([]);
-  const [isError, setIsError] = React.useState(false);
-  React.useEffect(() => {
+  const [songValue, setSongValue] = useState([]);
+  const [isError, setIsError] = useState(false);
+
+  useEffect(() => {
     const fetchdata = async (searchValue) => {
       if (searchValue === "" || searchValue !== songValue) {
         setSongValue([]);
       }
+
       if (searchValue !== "") {
         const options = {
           method: "GET",
@@ -28,64 +30,70 @@ const GetMovieAppResults = () => {
             numberOfTopResults: "5",
           },
           headers: {
-            "X-RapidAPI-Key":
-              "70143dc9c7msh6ff1068fea1b043p146d23jsn3df8381f9773",
+            "X-RapidAPI-Key": "70143dc9c7msh6ff1068fea1b043p146d23jsn3df8381f9773",
             "X-RapidAPI-Host": "spotify23.p.rapidapi.com",
           },
         };
+
         try {
           const response = await axios.request(options);
           let arr = [];
-          let obj = {};
 
           Object.entries(response.data).forEach(([key, data]) => {
-            obj = {
+            let obj = {
               title: key,
+              name: [],
             };
-            obj.name = [];
-            // console.log("data",data.items)
+
             data.items.forEach((item) => {
-            //  console.log("item",item)
-              
               Object.values(item).forEach((value) => {
-                // console.log(value.name);
                 obj.name.push(value.name);
               });
-              
-              
             });
+
             arr.push(obj);
-            obj = {};
           });
+
           setSongValue(arr);
         } catch (error) {
           console.error(error.response);
           setIsError(true);
-          // setErrorResult(error);
         }
       }
     };
+
     fetchdata(searchSongValue);
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [searchSongValue]);
-  console.log(songValue);
+
   const displaySongs = () => {
     return (
       <>
+        <Card title="Card title" bordered={true}>
+          card title
+        </Card>
         {songValue.length !== 0 ? (
-          <>
-            <Row gutter={16}>
-              <Col span={6}>
-                <ListComp data={songValue} className="listResult" />
-              </Col>
-            </Row>
-          </>
+          songValue.map((item) => (
+            <React.Fragment key={item.title}>
+              {item.title}
+              {item.name.map((val) => (
+                <Row gutter={16} key={val}>
+                  <Col span={8}>
+                    <Card title="Card title" bordered={true}>
+                      {val}
+                    </Card>
+                  </Col>
+                </Row>
+              ))}
+            </React.Fragment>
+          ))
         ) : (
           <Loader />
         )}
       </>
     );
   };
+
   return (
     <>
       {searchSongValue !== "" && displaySongs()}
